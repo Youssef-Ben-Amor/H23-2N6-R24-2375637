8152e80d87de194a878dffae36c0f7632aadbe79
Youssef-Ben-Amor <2375637@cegepmontpetit.ca>
2024-04-24 10:59:40 -0400

    Exercice 6 fait

---
+++ R24 Polymorphisme (Matériel) H23.1.0/Exer06 MessageCode/MessageCode/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageCode
+{
+    public abstract class Message
+    {
+        public string MessageNonEncodé { get; protected set;}
+        public abstract string MessageEncodé { get; }
+        public Message(string pMessageNonEncodé) { MessageNonEncodé = pMessageNonEncodé; }
+        public override string ToString() { return MessageEncodé; }
+
+    }
+}
